b7b69f37d4d0e4fd52045699067e4f2f83e156af
JohanTran <johan.tran0210@gmail.com>
2026-08-31 18:28:01 +0200

    chore: format files

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -4,5 +4,5 @@ import react from "ultracite/oxlint/react";
 
 export default defineConfig({
   extends: [core, react],
-  ignorePatterns: [...(core.ignorePatterns ?? []), "**/.vscode/**",],
+  ignorePatterns: [...(core.ignorePatterns ?? []), "**/.vscode/**"],
 });
