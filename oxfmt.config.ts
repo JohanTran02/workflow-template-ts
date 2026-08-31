e2dc4bf3270f95ae1cbf441dec6fa958d9995afd
JohanTran <johan.tran0210@gmail.com>
2026-08-31 19:36:01 +0200

    ci(oxfmt): change oxfmt settings

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -4,4 +4,5 @@ import ultracite from "ultracite/oxfmt";
 export default defineConfig({
   ...ultracite,
   ignorePatterns: [...(ultracite.ignorePatterns ?? []), "**.vscode/**"],
+  insertFinalNewline: false,
 });
